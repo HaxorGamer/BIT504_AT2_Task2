cf471866d4c1456bbf493f7f8f470d3a3b4fb2c2
HaxorGamer <g4m3r.forty2@hotmail.com>
2023-01-05 12:40:20 +1300

    added comments to the code

--- BIT504_AT2_Task2/src/Board.java
+++ BIT504_AT2_Task2/src/Board.java
@@ -12,7 +12,7 @@ public class Board {
 	/** Constructor to create the game board */
 	public Board() {
 		
-	 //TODO: initialise the cells array using ROWS and COLS constants 
+	 //set up the cells to match the size of the constants from GameMain for both ROWS and COLS
 		cells = new Cell[GameMain.ROWS][GameMain.COLS];
 		for (int row = 0; row < GameMain.ROWS; ++row) {
 			for (int col = 0; col < GameMain.COLS; ++col) {
@@ -27,9 +27,8 @@ public class Board {
 	public boolean isDraw() {
 		
 		
-		// TODO: Check whether the game has ended in a draw. 
-		// Hint: Use a nested loop (see the constructor for an example). Check whether any of the cells content in the board grid are Player.Empty. If they are, it is not a draw.
-		// Hint: Return false if it is not a draw, return true if there are no empty positions left
+		// this section should run through each cell in the game and check if the value is set to Player.Empty
+		// if there are any matches the game continues if theres no match it should update the gamestate to a draw (gamestate is handled by GameMain)
 		for (int row = 0; row < GameMain.ROWS; row++) {
 			for(int col = 0; col <GameMain.COLS; col++) {
 				if (cells[row][col].content == Player.Empty) {
@@ -47,9 +46,9 @@ public class Board {
 		if(cells[playerRow][0].content == thePlayer && cells[playerRow][1].content == thePlayer && cells[playerRow][2].content == thePlayer )
 			return true; 
 		
-		 // TODO: Check if the player has 3 in the playerCol.
-		 // Hint: Use the row code above as a starting point, remember that it goes cells[row][column] 
-		if(cells[playerCol][0].content == thePlayer && cells[playerCol][1].content == thePlayer && cells[playerCol][2].content == thePlayer )
+		 // 
+		 //  
+		if(cells[0][playerCol].content == thePlayer && cells[1][playerCol].content == thePlayer && cells[2][playerCol].content == thePlayer )
 			return true;
 		
 		
